bff37ccf8c2e402d9bbaff9876aac9e032260e8b
samkallon <39991473+samkallon@users.noreply.github.com>
2023-02-28 18:07:32 +0800

    添加shaderToy

--- src/components/ShaderToy/ShaderToy.vue
+++ src/components/ShaderToy/ShaderToy.vue
@@ -24,13 +24,303 @@ const init = () => {
   if (!viewer) return
   clear()
   message.info('数据加载中!')
+  const pointList = [
+    {
+      "longitude": 2.1062620701276042,
+      "latitude": 0.546721644563287,
+      "height": 7.995518221652699
+    },
+    {
+      "longitude": 2.1062490076441174,
+      "latitude": 0.546706115011185,
+      "height": 8.001359856988994
+    },
+    {
+      "longitude": 2.1062434828642616,
+      "latitude": 0.546684978685106,
+      "height": 7.995644627565386
+    },
+    {
+      "longitude": 2.106250521714073,
+      "latitude": 0.5466547848477626,
+      "height": 8.00310781276061
+    },
+    {
+      "longitude": 2.106230423786619,
+      "latitude": 0.5466465878971495,
+      "height": 8.002182259574052
+    },
+    {
+      "longitude": 2.1062319327849757,
+      "latitude": 0.5466349417316715,
+      "height": 8.003818925223515
+    },
+    {
+      "longitude": 2.1062374637095953,
+      "latitude": 0.5466163943582998,
+      "height": 8.03622025419552
+    },
+    {
+      "longitude": 2.1062872358115556,
+      "latitude": 0.5465974202485588,
+      "height": 8.838584640408591
+    },
+    {
+      "longitude": 2.1062642324769163,
+      "latitude": 0.5465702707488097,
+      "height": 11.343846009574232
+    },
+    {
+      "longitude": 2.106277177559484,
+      "latitude": 0.5465236666954383,
+      "height": 8.373569884180709
+    },
+    {
+      "longitude": 2.106277726724487,
+      "latitude": 0.5464472076753544,
+      "height": 7.543681825361595
+    },
+    {
+      "longitude": 2.1062826518690754,
+      "latitude": 0.5464206627020793,
+      "height": 7.53801450286331
+    },
+    {
+      "longitude": 2.1062580233987847,
+      "latitude": 0.5464173685100093,
+      "height": 17.822017931263193
+    },
+    {
+      "longitude": 2.1062598461095945,
+      "latitude": 0.5464017675670739,
+      "height": 7.550978639457115
+    },
+    {
+      "longitude": 2.1062421144288184,
+      "latitude": 0.5463840639295755,
+      "height": 9.987836242972373
+    },
+    {
+      "longitude": 2.1062496757694684,
+      "latitude": 0.5463690599135143,
+      "height": 7.073108350921812
+    },
+    {
+      "longitude": 2.1062934373514155,
+      "latitude": 0.5463674084265577,
+      "height": 7.56011214018234
+    },
+    {
+      "longitude": 2.10631684509339,
+      "latitude": 0.5463753175777318,
+      "height": 7.5610873990568095
+    },
+    {
+      "longitude": 2.1063598966596047,
+      "latitude": 0.5463729244509468,
+      "height": 7.582717292639772
+    },
+    {
+      "longitude": 2.106403797977494,
+      "latitude": 0.5463725860529461,
+      "height": 7.584523321214339
+    },
+    {
+      "longitude": 2.106429057319793,
+      "latitude": 0.5463809202208978,
+      "height": 7.579116699016599
+    },
+    {
+      "longitude": 2.106454968117248,
+      "latitude": 0.546369429932468,
+      "height": 7.592128086947539
+    },
+    {
+      "longitude": 2.1065025063413207,
+      "latitude": 0.5463760965848969,
+      "height": 7.607323467944809
+    },
+    {
+      "longitude": 2.10653775181678,
+      "latitude": 0.5464026378669868,
+      "height": 7.624874024661941
+    },
+    {
+      "longitude": 2.1065764702910443,
+      "latitude": 0.5463980620392106,
+      "height": 7.641191261894836
+    },
+    {
+      "longitude": 2.106594354649676,
+      "latitude": 0.5463795562360878,
+      "height": 7.637728170977547
+    },
+    {
+      "longitude": 2.1065866657400996,
+      "latitude": 0.5463509316795951,
+      "height": 7.646588005769031
+    },
+    {
+      "longitude": 2.106617253145736,
+      "latitude": 0.5463336438277276,
+      "height": 7.955285600151329
+    },
+    {
+      "longitude": 2.106669502191231,
+      "latitude": 0.5463240843441537,
+      "height": 7.664317405963897
+    },
+    {
+      "longitude": 2.1067084291106184,
+      "latitude": 0.5463132971082825,
+      "height": 9.158310425142249
+    },
+    {
+      "longitude": 2.1067762295802606,
+      "latitude": 0.5463213663620893,
+      "height": 12.842765682443746
+    },
+    {
+      "longitude": 2.1067956658561453,
+      "latitude": 0.5463215759277888,
+      "height": 14.929496950416418
+    },
+    {
+      "longitude": 2.106823593748781,
+      "latitude": 0.5463551038458163,
+      "height": 8.574602944478142
+    },
+    {
+      "longitude": 2.1068191975540196,
+      "latitude": 0.5463764062902823,
+      "height": 9.768464599462162
+    },
+    {
+      "longitude": 2.1068007748489355,
+      "latitude": 0.5463826079115675,
+      "height": 7.707642823372926
+    },
+    {
+      "longitude": 2.106802279462026,
+      "latitude": 0.5464094994908457,
+      "height": 12.159453366842508
+    },
+    {
+      "longitude": 2.10679155720928,
+      "latitude": 0.5464630922982245,
+      "height": 8.648055837110745
+    },
+    {
+      "longitude": 2.1067905812809116,
+      "latitude": 0.5465157125284849,
+      "height": 7.656844269262984
+    },
+    {
+      "longitude": 2.106819282248326,
+      "latitude": 0.5465270633262507,
+      "height": 7.68994951814912
+    },
+    {
+      "longitude": 2.1068768006294425,
+      "latitude": 0.5465231532556114,
+      "height": 13.036261593602719
+    },
+    {
+      "longitude": 2.1068686843553683,
+      "latitude": 0.5465367610328031,
+      "height": 16.3629366169265
+    },
+    {
+      "longitude": 2.1068132291945454,
+      "latitude": 0.5465539307205138,
+      "height": 9.518716447042769
+    },
+    {
+      "longitude": 2.1067562184113955,
+      "latitude": 0.5465754060148078,
+      "height": 7.991731570499541
+    },
+    {
+      "longitude": 2.106692926461611,
+      "latitude": 0.5466313381718763,
+      "height": 13.625187857682398
+    },
+    {
+      "longitude": 2.1066667720665797,
+      "latitude": 0.5466761708966694,
+      "height": 13.436594971445242
+    },
+    {
+      "longitude": 2.106612755919966,
+      "latitude": 0.5466801931847152,
+      "height": 11.851013996906332
+    },
+    {
+      "longitude": 2.1065524346555797,
+      "latitude": 0.5466679618214583,
+      "height": 7.6038203866436715
+    },
+    {
+      "longitude": 2.106548307281476,
+      "latitude": 0.5467168395813848,
+      "height": 17.83160328288637
+    },
+    {
+      "longitude": 2.106472978376953,
+      "latitude": 0.5467479916309212,
+      "height": 18.811393914000384
+    },
+    {
+      "longitude": 2.10639416280127,
+      "latitude": 0.5467516711842298,
+      "height": 7.69610165086814
+    },
+    {
+      "longitude": 2.1063683521265784,
+      "latitude": 0.5467567638243167,
+      "height": 7.662227762788265
+    },
+    {
+      "longitude": 2.106362036767062,
+      "latitude": 0.5467802772392484,
+      "height": 14.327750808005511
+    },
+    {
+      "longitude": 2.1063345973396705,
+      "latitude": 0.5467913615562383,
+      "height": 9.147668603887828
+    },
+    {
+      "longitude": 2.1063078839383733,
+      "latitude": 0.5467934200418613,
+      "height": 12.360767344032329
+    },
+    {
+      "longitude": 2.1062873475213437,
+      "latitude": 0.546765898367516,
+      "height": 17.919455656290346
+    },
+    {
+      "longitude": 2.1062879238490724,
+      "latitude": 0.5467400577718533,
+      "height": 7.52727417503945
+    }
+  ]
 
-  var box = new Cesium.BoxGeometry({
+  const PolygonHierarchy = []
+  pointList.map(e=>{
+    PolygonHierarchy.push(Cesium.Math.toDegrees(e.longitude))
+    PolygonHierarchy.push(Cesium.Math.toDegrees(e.latitude))
+    PolygonHierarchy.push(e.height + 1000)
+  })
+  var box = new Cesium.PolygonGeometry({
     vertexFormat : Cesium.VertexFormat.POSITION_NORMAL_AND_ST,
-    maximum : new Cesium.Cartesian3(250000.0, 250000.0, 250000.0),
-    minimum : new Cesium.Cartesian3(-250000.0, -250000.0, -250000.0),
+    polygonHierarchy:new Cesium.PolygonHierarchy(
+        Cesium.Cartesian3.fromDegreesArrayHeights(PolygonHierarchy)
+    ),
+    height:10
   });
-  const geometry = Cesium.BoxGeometry.createGeometry(box);
+  const geometry = Cesium.PolygonGeometry.createGeometry(box);
 
   let inst = new Cesium.GeometryInstance({
     geometry: geometry,
@@ -238,23 +528,13 @@ const init = () => {
       }
                 `,
   });
-
-  let modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-      Cesium.Cartesian3.fromDegrees(110, 40, 10)
-  );
-
   viewer.scene.primitives.add(new Cesium.Primitive({
     geometryInstances: inst,
     appearance: aper,
-    modelMatrix: modelMatrix,
     asynchronous : false
   }));
 
-  viewer.camera.flyToBoundingSphere(new Cesium.BoundingSphere(
-      Cesium.Cartesian3.fromDegrees(110, 40, 10), 950000,
-  ),{
-    duration: 0.1,
-  });
+  viewer.camera.flyTo({destination:Cesium.Cartesian3.fromDegrees(120.69272763, 31.30999739,7000)})
 
   function renderLoop(timestamp:any){
     aper.material.uniforms.iTime = timestamp/1000;
